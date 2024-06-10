bc594d0e07c7ec1a3e6601484df3e7947ed78882
axie10 <cryptosushi21@gmail.com>
2024-06-10 17:32:38 +0200

    empezando a usar el tipo de dato any en vez de interface{}

--- src/genericos en Go/main.go
+++ src/genericos en Go/main.go
@@ -1,5 +1,27 @@
 package main
 
+import(
+	"fmt"
+)
+
+/**Aqui estamos utilizando la propiedad 
+ * interface para impirmir diferentes tipos de datos y
+ * despues vamos a utilizar el tipo de dato any
+ * para imprimir el resto de tipos de datos*/
+func PrintList(list ...interface{}){
+	for _,c := range list {
+		fmt.Printf("%T - %v\n", c, c)
+	}
+}
+func PrintListAny(list ...any){
+	for _,c := range list {
+		fmt.Printf("%T - %d\n", c, c)
+	}
+}
+
 func main(){
-	
+
+	PrintList("pepe", 27, 15, true, "caratula")
+	PrintListAny("pepe", 27, 15, true, "caratula")
+
 }
