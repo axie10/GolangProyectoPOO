41b0a93e2aa9d58a64a59d7fe4178afdb9a91ac8
axie10 <cryptosushi21@gmail.com>
2024-06-12 09:21:14 +0200

    Terminamos la aplicacion de wiki mostramos las paginas podemos editar las paginas y manejamos los errores

--- src/gowiki/wiki.go
+++ src/gowiki/wiki.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	// "fmt"
-	"fmt"
+	// "fmt"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"text/template"
 )
 
 type Page struct {
@@ -29,9 +32,68 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hola, %q, primera vez que levantamos go en un cliente(navegador). Capturamos la url: %s", "golanger", r.URL.Path[1:])
+/** */
 
+/**Creamos una variable global donde vamos a validar el path */
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Titulo de pagina invalido")
+	}
+	return m[2], nil
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+/**variable para cargar las diferentes views de mi pagina web */
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+/**Funcion comun para renderizar las paginas y no tener que ir llamando 
+ * una a una */
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
@@ -40,8 +102,9 @@ func main() {
 	// p2, _ := loadPage("TestPage")
 	// fmt.Println(string(p2.Body))
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
